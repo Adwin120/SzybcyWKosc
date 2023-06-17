823a5b837d32de560064273d2a749b30486fc006
JanRucinski <jasislav@gmail.com>
2023-06-17 18:37:32 +0200

    splitscreen add

--- Assets/Player/PlayerKartController.cs
+++ Assets/Player/PlayerKartController.cs
@@ -1,31 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerKartController : MonoBehaviour
 {
     // Start is called before the first frame update
     private KartController kartController;
+    [SerializeField] PlayerInput playerInput;
 
     void Start()
     {
         kartController = GetComponentInChildren<KartController>();
+        playerInput = GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
     private bool lastJumpState = false;
     void Update()
     {
-        float motor = Input.GetAxis("Vertical");
-        float steering = Input.GetAxis("Horizontal");
+        float steering = playerInput.actions["Move"].ReadValue<Vector2>().x;
+        float motor = playerInput.actions["Move"].ReadValue<Vector2>().y;
+        //float motor = Input.GetAxis("Vertical");
+        //float steering = Input.GetAxis("Horizontal");
         kartController.Drive(motor);
         kartController.Turn(steering);
-        if (Input.GetAxis("Jump") > 0 && lastJumpState == false)
+        if (playerInput.actions["Drift"].IsPressed() && lastJumpState == false)
         {
             kartController.StartDrift();
             lastJumpState = true;
         }
-        if (Input.GetAxis("Jump") == 0 && lastJumpState == true)
+        else
         {
             kartController.StopDrift();
             lastJumpState = false;
