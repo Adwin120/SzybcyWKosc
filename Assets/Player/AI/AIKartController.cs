fb0e7c326a5f245a89cd7f3eaa46e312ec8ab0e8
JanRucinski <jasislav@gmail.com>
2023-06-22 23:50:35 +0200

    idk

--- Assets/Player/AI/AIKartController.cs
+++ Assets/Player/AI/AIKartController.cs
@@ -8,24 +8,24 @@ public class AIKartController : MonoBehaviour
     // Start is called before the first frame update
     private KartController kartController;
     private PlayerData playerData;
-    public Node currentNode;
+    public Waypoint currentWaypoint;
     public NavMeshAgent agent;
 
     void Start()
     {
         kartController = GetComponentInChildren<KartController>();
         playerData = GetComponentInChildren<PlayerData>();
-        currentNode = GameObject.Find("NodeStart").GetComponent<Node>();
+        currentWaypoint = GameObject.Find("NodeStart").GetComponent<Waypoint>();
         //playerInput = GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(currentNode.nextNode.transform.position);
+        agent.SetDestination(currentWaypoint.nextWaypoint.transform.position);
         if(agent.remainingDistance < 10)
         {
-            currentNode = currentNode.nextNode;
+            currentWaypoint = currentWaypoint.nextWaypoint;
         }
         //float steering = playerInput.actions["Move"].ReadValue<Vector2>().x;
         //float motor = playerInput.actions["Move"].ReadValue<Vector2>().y;
