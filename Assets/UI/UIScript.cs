967465e01e92d29a7f66fd56be46d2efb4936a0c
Adwin120 <adam.winnik120@gmail.com>
2023-06-22 19:50:50 +0200

    fix ui for split screen

--- Assets/UI/UIScript.cs
+++ Assets/UI/UIScript.cs
@@ -2,22 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.UI;
+using static UnityEngine.InputSystem.InputAction;
 
-[RequireComponent(typeof(PlayerInput))]
 public class UIScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject pauseMenu;
+
+    private InputSystemUIInputModule input;
     void Start()
     {
-        pauseMenu.SetActive(false);
+        //pauseMenu.SetActive(false);
+        input = this.GetComponent<InputSystemUIInputModule>();
+        input.cancel.action.performed += OnCancelCallback;
 
+    }
+
+    private void OnDestroy()
+    {
+        input.cancel.action.performed -= OnCancelCallback;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    void OnCancelCallback(CallbackContext context)
+    {
+        OnCancel();
     }
 
     void OnCancel()
